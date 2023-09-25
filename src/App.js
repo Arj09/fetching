e4421ec986e79d3db86415d1aed80da3bfd72146
Arj09 <arjunkushwah100199@gmail.com>
2023-09-25 13:57:34 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import  axios   from 'axios'
+import Box from '@mui/material/Box';
+import { Button, Card, CardActions, CardContent, CardMedia, Stack, Typography } from '@mui/material';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+
+  //fetching data 
+  useEffect(()=>{
+    axios.get('https://api.punkapi.com/v2/beers').then((res)=>{
+      console.log(res.data)
+      setData(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Box  sx={{width:'80vw', margin:"20px auto"}}>
+      
+      {
+        data.map((data)=>{
+          return(
+
+
+            <Card sx={{ width:{xs:"100%", md:"60%"}, margin:"20px auto"}}>
+        <CardMedia 
+          sx={{ height: '5%' , width:"10%", justifyContent:"center", margin:"10px auto"}}
+          component="img"
+          height="10"
+          image= {data.image_url}
+          title="green iguana"
+          />
+
+          <CardContent>
+            <Typography gutterBottom   variant="h5" component="div">{data.name}</Typography>
+            <Typography variant='body2'color="text.secondary">{data.description}</Typography>
+          </CardContent>
+          <Stack direction={'column'} spacing={1} sx={{margin:"10px 30px"}}>
+            <Typography variant='body2'>{`Boil valume value : ${data.boil_volume.value}`}</Typography>
+            <Typography variant='body2'>{`Tagline : ${data.tagline}`}</Typography>
+          </Stack>
+
+      </Card>
+          )
+        })
+      }
+
+    </Box>
+    
+
+    
+
+    
+
+    
+
+    
+    </>
   );
 }
 
